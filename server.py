385ee90f723cfc62fdcf659803d71208ef8778de
Kavin Parikh <kpk1611@gmail.com>
2023-04-13 10:48:05 -0400

    updating the server.py for the client code

--- server.py
+++ server.py
@@ -1,57 +1,95 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 12 14:49:26 2023
-
-@author: kpk16
-"""
-
+import riffusion
 import socket
 import threading
+import time
 
-# Maximum number of connections allowed
+# Constants
+SERVER_ADDRESS = 'localhost'
+SERVER_TCP_PORT = 2351
+SERVER_UDP_PORT = 2352
 MAX_CONNECTIONS = 50
+AUDIO_BUFFER_SIZE = 1024
 
-# Maximum number of game instances allowed
-MAX_GAME_INSTANCES = 1
+# Global variables
+clients = []
+prompt1 = ''
+prompt2 = ''
+votes = {'good': 0, 'bad': 0, 'prompt1': 0, 'prompt2': 0}
 
-# List of active game instances
-active_game_instances = []
+# Riffusion library initialization
+riffusion.init()
 
-# Function to handle client connections
-def handle_client_connection(conn, addr):
-    print(f"New client connected: {addr}")
-    # TODO: Implement game logic and audio transmission
-    conn.close()
-    print(f"Client disconnected: {addr}")
+# TCP server thread
+def tcp_server_thread():
+    # Initialize TCP socket
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.bind((SERVER_ADDRESS, SERVER_TCP_PORT))
+    tcp_socket.listen(MAX_CONNECTIONS)
+    print(f'TCP server listening on {SERVER_ADDRESS}:{SERVER_TCP_PORT}...')
 
-# Function to start the server
-def start_server():
-    # Create a TCP socket and bind it to a specific port
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 2351))
-    server_socket.listen()
+    # Accept new connections
+    while True:
+        client_socket, client_address = tcp_socket.accept()
+        print(f'New client connected: {client_address}')
+        clients.append(client_socket)
+        threading.Thread(target=handle_tcp_client, args=(client_socket,)).start()
 
-    print("Server listening on port 2351")
-
-    # Keep accepting new connections until the maximum number of connections is reached
-    while len(active_game_instances) < MAX_GAME_INSTANCES:
-        try:
-            conn, addr = server_socket.accept()
-            # Check if the maximum number of connections has been reached
-            if len(active_game_instances) >= MAX_GAME_INSTANCES:
-                conn.send(b"Server is busy. Please try again later.")
-                conn.close()
-                print(f"Rejected client connection: {addr}")
-                continue
-            # Start a new thread to handle the client connection
-            client_thread = threading.Thread(target=handle_client_connection, args=(conn, addr))
-            client_thread.start()
-        except KeyboardInterrupt:
-            # Stop the server if the user presses Ctrl-C
+# Handle TCP client requests
+def handle_tcp_client(client_socket):
+    while True:
+        data = client_socket.recv(1024).decode()
+        if not data:
             break
+        if data.startswith('prompt'):
+            prompt = data.split()[1]
+            if not prompt1:
+                prompt1 = prompt
+            else:
+                prompt2 = prompt
+            print(f'Received prompt: {prompt}')
+        elif data == 'guess':
+            if riffusion.is_playing():
+                prompt = riffusion.get_prompt()
+                for client in clients:
+                    client.send(prompt.encode())
+        elif data.startswith('invite'):
+            invitation = data.split()[1]
+            print(f'Received invitation: {invitation}')
+        elif data.startswith('vote'):
+            vote = data.split()[1]
+            if vote == 'good':
+                votes['good'] += 1
+            elif vote == 'bad':
+                votes['bad'] += 1
+            elif vote == 'prompt1':
+                votes['prompt1'] += 1
+            elif vote == 'prompt2':
+                votes['prompt2'] += 1
+            print(f'Received vote: {vote}')
 
-    # Close the server socket when the maximum number of game instances has been reached
-    server_socket.close()
+# UDP audio server thread
+def udp_server_thread():
+    # Initialize UDP socket
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind((SERVER_ADDRESS, SERVER_UDP_PORT))
+    print(f'UDP server listening on {SERVER_ADDRESS}:{SERVER_UDP_PORT}...')
 
-# Start the server
-start_server()
+    # Continuously send audio data to all connected clients
+    while True:
+        data = riffusion.get_audio_buffer(AUDIO_BUFFER_SIZE)
+        for client in clients:
+            try:
+                client.sendall(data)
+            except socket.error:
+                clients.remove(client)
+
+# Background graphics thread
+def background_graphics_thread():
+    while True:
+        time.sleep(5)
+        print(f'Votes: good={votes["good"]}, bad={votes["bad"]}, prompt1={votes["prompt1"]}, prompt2={votes["prompt2"]}')
+
+# Start server threads
+threading.Thread(target=tcp_server_thread).start()
+threading.Thread(target=udp_server_thread).start()
+threading.Thread(target=background_graphics_thread).start()
